524b93a2096fc64343e0759a152bf3e9fff94396
Ilya_Sedelnikov <illyasedelnikov123@gmail.com>
2024-04-19 15:57:53 +0200

    selecting data using ResultSet with JDBC

--- src/main/java/com/project/jdbc/starter/JdbcRunner.java
+++ src/main/java/com/project/jdbc/starter/JdbcRunner.java
@@ -4,14 +4,28 @@ import com.project.jdbc.starter.util.ConnectionManager;
 import org.postgresql.Driver;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class JdbcRunner {
     public static void main(String[] args) throws SQLException {
         Class<Driver> driver = Driver.class;
+        String sql = """
+                SELECT * FROM info;
+                """;
 
-        try (Connection connection = ConnectionManager.open()) {
+        try (Connection connection = ConnectionManager.open();
+             Statement statement = connection.createStatement()) {
             System.out.println(connection.getTransactionIsolation());
+            ResultSet resultSet = statement.executeQuery(sql);
+
+            System.out.println(resultSet);
+
+            while (resultSet.next()) {
+                System.out.println(resultSet.getLong("id"));
+                System.out.println(resultSet.getString("data"));
+            }
         }
     }
 }
