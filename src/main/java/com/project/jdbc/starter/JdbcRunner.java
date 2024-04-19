80308f1741e9a67c7a592c188ca5573794330feb
Ilya_Sedelnikov <illyasedelnikov123@gmail.com>
2024-04-19 14:49:54 +0200

    reading data from configuration (.properties) file

--- src/main/java/com/project/jdbc/starter/JdbcRunner.java
+++ src/main/java/com/project/jdbc/starter/JdbcRunner.java
@@ -1,12 +1,14 @@
 package com.project.jdbc.starter;
 
 import com.project.jdbc.starter.util.ConnectionManager;
+import org.postgresql.Driver;
 
 import java.sql.Connection;
 import java.sql.SQLException;
 
 public class JdbcRunner {
     public static void main(String[] args) throws SQLException {
+        Class<Driver> driver = Driver.class;
 
         try (Connection connection = ConnectionManager.open()) {
             System.out.println(connection.getTransactionIsolation());
